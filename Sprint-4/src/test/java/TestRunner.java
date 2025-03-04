7853e0326ae6b8fdc41443b83cce20deafaca524
Alex Feudorova <alex.feudorova@gmail.com>
2025-03-04 17:39:53 +0300

    Delete Sprint-4 directory

--- Sprint-4/src/test/java/TestRunner.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import Parametrized.OrderTest;
-import Parametrized.QuestionTest;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({QuestionTest.class, OrderTest.class})
-public class TestRunner {
-}
